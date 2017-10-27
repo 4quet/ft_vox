95d00296abcc0bac02f0b9187b8255c20e3ee47c
Thibaut PIERRON <tpierron@e3r3p20.42.fr>
2017-10-27 11:39:31 +0200

    frame duration display added

--- main.cpp
+++ main.cpp
@@ -3,32 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thibautpierron <thibautpierron@student.    +#+  +:+       +#+        */
+/*   By: tpierron <tpierron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/23 09:23:13 by tpierron          #+#    #+#             */
-/*   Updated: 2017/10/26 13:24:31 by lfourque         ###   ########.fr       */
+/*   Updated: 2017/10/27 10:59:51 by tpierron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_vox.hpp"
 
-static void     UIManager(Action::Enum action, ChunkManager & chunkManager) {
+static void     UIManager(Action::Enum action, int frameDuration, ChunkManager & chunkManager) {
     static bool debugSwitch = false;
     GLString info;
-
+    
     if (action == Action::DEBUG)
         debugSwitch = !debugSwitch;
         
     if(debugSwitch) {
         std::string totalBlock = std::to_string(chunkManager.getTotalActiveBlocks());
         std::string totalChunk = std::to_string(chunkManager.getTotalActiveChunks());
-        info.renderText("Total blocks: " + totalBlock, 50, 50, glm::vec3(0.2f, 0.7f, 1.f));
+
+        info.renderText("frameDuration: " + std::to_string(frameDuration), 50, 150, glm::vec3(0.2f, 0.7f, 1.f));
         info.renderText("Total chunks: " + totalChunk, 50, 100, glm::vec3(0.2f, 0.7f, 1.f));
+        info.renderText("Total blocks: " + totalBlock, 50, 50, glm::vec3(0.2f, 0.7f, 1.f));
     }
 }
 
 int     main() {
-    
+    clock_t start, end;
     Sdl_gl_win window;
 
     Action::Enum action = Action::NONE;
@@ -39,18 +41,21 @@ int     main() {
     Shader	shader("src/shaders/basic_vertex_shader.glvs", "src/shaders/basic_fragment_shader.glfs");
     Skybox  skybox("skybox");
     
+    start = 0;
     while(action != Action::ESCAPE) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         action = window.eventManager();
         
         camera.move(action, window.getMouseX(), window.getMouseY());
         shader.setCamera(camera.getMatrix());
-
+        
 		m.update(shader);
-
         skybox.draw();
-        UIManager(action, m);
+        
+        end = std::clock();
+        UIManager(action, difftime( end, start ), m);
         SDL_GL_SwapWindow(window.getWin());
+        start = std::clock();
     }
     return 0;
 }
